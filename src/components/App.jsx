967bd257aa164dcb0e13f374952791d32a8856b3
ILLIA89 <anolij1@gmail.com>
2023-09-20 21:40:00 +0200

    feedback-task 1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import css from './App.module.css';
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,8 @@
+export const Section = ({ title, children }) => {
+  return (
+    <div>
+      {/* <h1 className={css.section_title}>{title}</h1> */}
+      {children}
+    </div>
+  );
+};
